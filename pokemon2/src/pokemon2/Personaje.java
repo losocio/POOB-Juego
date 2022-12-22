3de1ef74d55452fee25478ca62d3f7cda37384ed
OscarMaestre987 <118920861+OscarMaestre987@users.noreply.github.com>
2022-12-22 16:39:40 +0100

    Add files via upload
    esto es una nueva carpeta en la que estaba programando lo que te dije en discord, no me parecía necesario que hubiera tantas clases así que los quite.

---
+++ pokemon2/src/pokemon2/Personaje.java
@@ -0,0 +1,20 @@
+package pokemon2;
+
+public abstract class Personaje {
+
+	private int vida;
+	private int ataque;
+	private int def;
+	private int velocidad;
+	private String raza;
+	
+	public Personaje(String raza, int vida, int ataque, int def, int velocidad) {
+		this.raza=raza;
+		this.vida=vida;
+		this.ataque=ataque;
+		this.def=def;
+		this.velocidad=velocidad;
+	}
+	
+	
+}
