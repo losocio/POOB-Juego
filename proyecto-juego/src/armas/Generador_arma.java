2513b5b1d1a336683d034046acf9b4ac508c9a1f
Álvaro de Ocio <losocio143@gmail.com>
2023-01-02 18:26:39 +0100

    Generador arma CPU

--- proyecto-juego/src/armas/Generador_arma.java
+++ proyecto-juego/src/armas/Generador_arma.java
@@ -1,5 +1,34 @@
 package armas;
 
-public class Generador_arma {
+import java.util.Random;
 
+public class Generador_arma {
+	private String armas[] = {"Arco","Daga","Espada","Lanza","Martillo"};
+	private Arma arma;
+	
+	private int getRandom(int min, int max) {
+		Random random = new Random();
+		return random.nextInt((max - min) + 1) + min;
+	}
+	
+	public Arma getArmaCPU() {
+		switch(armas[getRandom(0, 4)]) {
+			case "Arco":
+				arma = new Arco();
+				break;
+			case "Daga":
+				arma = new Daga();
+				break;
+			case "Espada":	
+				arma = new Espada();
+				break;
+			case "Lanza":
+				arma = new Lanza();
+				break;
+			case "Martillo":
+				arma = new Martillo();
+				break;
+		}
+		return arma;
+	}
 }
